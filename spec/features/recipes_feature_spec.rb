13c776afd71a72ee5b4596a5c3ac668d7130ff1b
Winifred Au <winifredau1990@gmail.com>
2015-11-20 11:34:17 -0500

    wrote test for adding recipes

--- spec/features/recipes_feature_spec.rb
+++ spec/features/recipes_feature_spec.rb
@@ -8,4 +8,16 @@ feature 'recipes' do
       expect(page).to have_link 'Add a recipe'
     end
   end
+
+  context 'recipes have been added' do
+    before do
+      Recipe.create(id: '1')
+    end
+    scenario 'display recipes' do
+      visit '/recipes'
+      expect(page).to have_content('Your Recipes')
+      expect(page).not_to have_content('No recipes yet')
+    end
+  end
+  
 end

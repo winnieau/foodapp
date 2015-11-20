b21bfc37bfccffa9ddc47f6ce287d8f6f4ee9d15
Winifred Au <winifredau1990@gmail.com>
2015-11-20 12:44:51 -0500

    displaying recipes

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,7 +1,12 @@
 class RecipesController < ApplicationController
-
   def index
-    
+    @recipes = Recipe.all
+  	@search_term = params[:looking_for] || 'chocolate'
+  	# @recipes = Recipe.for(@search_term)
   end
 
+  # def create
+  #   Recipe.create(params[:recipe])
+  #   redirect_to '/recipes'
+  # end
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,11 @@
 class Recipe < ActiveRecord::Base
+  include HTTParty
+ ENV["FOOD2FORK_KEY"] = '94fd4f3733b383b5c28c9b2e0e5dda1e'
+ base_uri 'http://food2fork.com/api'
+ default_params key: ENV["FOOD2FORK_KEY"]
+ format :json
+
+ def self.for term
+ get("/search", query: { q: term})["recipes"]
+ end
 end
